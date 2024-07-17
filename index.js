6944fa9c6da5c9aa2539813aa9441c094e6b5af7
doyelee0313 <doyelee0313@gmail.com>
2024-07-18 01:20:04 +0900

    till chp3

---
+++ index.js
@@ -0,0 +1,45 @@
+// console 에 띄우기
+// console.log(`Hello`);
+// console.log(`I like pizza`);
+
+//팝업 알림창 띄우기
+// window.alert(`This is an alert`);
+// window.alert(`I like u`);
+
+//id로 getElement 해서 text로 바꾸기
+// document.getElementById("myH1").textContent = `Hwllo`;
+// document.getElementById("myP1").textContent = `Pizza`;
+
+//CHP2 VARIABLES
+
+//variable = container that stores a value
+
+//1. declaration let x;
+//2. assignment x = 100;
+
+//or x = 100;
+// let x;
+// x=100;
+// console.log(x);
+
+
+// let age = 25;
+// let price = 10.99;
+// let gpa = 2.1;
+
+// varable 출력할때는 ${} 이용해서 안에 넣기
+// console.log(`You are ${age} years old`);
+// console.log(`The price is $${price}`);
+// console.log(`your gpa is ${gpa}`);
+
+//무슨 타입인지 알고싶을때는 typeof
+//console.log(typeof gpa);
+
+// let fullName = "Bro";
+// let age = 25;
+
+// document.getElementById("p1").textContent = `Your name is ${fullName}`;
+// document.getElementById("p2").textContent = `Your age is ${age}`;
+
+//CHP4 ACCEPT USER INPUT
+
